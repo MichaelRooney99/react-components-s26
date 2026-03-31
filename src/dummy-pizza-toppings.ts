16804bb6564a8db2c1f3f366a1b6cc629686b5dc
tsteelematc <tsteele@madisoncollege.edu>
2026-03-31 14:39:00 -0500

    Our dummy pizza toppings...

---
+++ src/dummy-pizza-toppings.ts
@@ -0,0 +1,14 @@
+export const dummyPizzaToppingsFromApi = [
+  {
+    name: 'Pepperroni',
+    price: 3.50,
+  },
+  {
+    name: 'Sausage',
+    price: 4.25,
+  },
+  {
+    name: 'Extra Cheese',
+    price: 0.99,
+  },
+];
