1b762878c1c275ed94cd46ed2c5213b60212e195
THamilton1123 <thamilton12@madisoncollege.edu>
2026-04-13 17:58:17 -0500

    Created Carousel Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import PizzaToppings from './PizzaToppings';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PizzaToppings from "./PizzaToppings";
+import ThamiltonComponent from "./ThamiltonComponent";
 
 const App = () => {
-
   return (
-    <div
-      className='m-3'
-    >
-      <h1>
-        React Components
-      </h1>
+    <div className="m-3">
+      <h1>React Components</h1>
       <PizzaToppings />
+      <ThamiltonComponent />
     </div>
   );
-}
+};
 
-export default App
+export default App;
